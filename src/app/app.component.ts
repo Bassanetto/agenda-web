844ba5975ac3a13db3c7bd7b215450695758574f
Gustavo De Almeida Bassanetto <gustavo.almeidabassanetto@gmail.com>
2024-03-07 23:45:50 -0300

    Agenda Web :: Adiçaõ de sidebar e header ao projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
